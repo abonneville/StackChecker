0e8e9ddc74e12dfb5894a32b21424ea8439f2cd8
Andrew Bonneville <andrew.bonneville@gmail.com>
2019-09-28 15:40:44 -0700

    Implemetned detection of dispatch table

--- call_graph.py
+++ call_graph.py
@@ -4,12 +4,12 @@ import argparse
 
 import json
 
-from enum import auto, IntFlag
+from enum import auto, IntEnum
 
 import subprocess, sys
 
 
-class SymbolScope(IntFlag):
+class SymbolScope(IntEnum):
     index = 9
     none = auto()
     local = auto()
@@ -17,22 +17,22 @@ class SymbolScope(IntFlag):
     un_glb = auto()
     error = auto()
 
-class SymbolMessage(IntFlag):
+class SymbolMessage(IntEnum):
     index = 12
     none = auto()
     warning = auto()
 
-class SymbolType(IntFlag):
+class SymbolType(IntEnum):
     index = 15
     none = auto()
     function = auto()
     filename = auto()
     obj = auto()
 
-class NodeType(IntFlag):
+class NodeType(IntEnum):
     unknown = auto()
     function = auto()
-    func_ptr = auto()
+    dispatch_table = auto()
     vtable = auto()
 
 tool_path = "C:\Program Files (x86)\Atollic\TrueSTUDIO for STM32 9.2.0\ARMTools\\bin\\"
@@ -51,6 +51,7 @@ def is_valid_line(s):
         return True
     except ValueError:
         return False
+
 
 def is_function_start(s):
     """ Detects if the line indicates start of a new function
@@ -213,6 +214,75 @@ def build_symbols(filename):
 
             nodes.append(node)
 
+
+def build_symbol_table(symbols, filename):
+    """ Analysis the binary input file and create an internal list of symbols.
+    """
+    terminal = subprocess.Popen([cmd, '--syms', '--demangle', filename ], shell=True, 
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+    stdout, stderr = terminal.communicate()
+    lines = stdout.splitlines()
+
+    for line in lines:
+        address = 0
+        if( is_valid_line(line[0:8]) ):
+            symbol = {}
+
+            """ Colmun(s) at the beginning of the line are fixed width.
+            """
+
+            """ The input file contains a collection of all symbols; data, 
+                functions, etc...
+            """
+            if line[SymbolType.index] == ord('F'):
+                symbol['type'] = SymbolType.function
+            elif line[SymbolType.index] == ord('f'):
+                symbol['type'] = SymbolType.filename
+            elif line[SymbolType.index] == ord('O'):
+                symbol['type'] = SymbolType.obj
+            else:
+                symbol['type'] = SymbolType.none
+
+            # Grab 32-bit address
+            address = int(line[0:8], 16)
+
+            # Decode the symbol formatting
+            if line[SymbolScope.index] == ord('l'):
+                symbol['scope'] = SymbolScope.local
+            elif line[SymbolScope.index] == ord('g'):
+                symbol['scope'] = SymbolScope.glb
+            elif line[SymbolScope.index] == ord('u'):
+                symbol['scope'] = SymbolScope.un_glb
+            elif line[SymbolScope.index] == ord('!'):
+                symbol['scope'] = SymbolScope.error
+            else:
+                symbol['scope'] = SymbolScope.none
+
+            if line[SymbolMessage.index] == ord('W'):
+                symbol['message'] = SymbolMessage.warning
+            else:
+                symbol['message'] = SymbolMessage.none
+
+
+            """ The remainder of the columns are variable length word(s).
+            """
+            words = line[17:].split()
+            symbol['section'] = words[0]
+            symbol['size'] = int(words[1], 16)
+            try:
+                # Test, because some lines are missing the last field.
+                # Column width is 1 or more bytes
+                symbol['name'] = words[2:]
+            except IndexError:
+                # Column width is 0 bytes
+                symbol['name'] = ""
+
+            symbols[address] = symbol
+        
+        
+
+
 def build_nodes(lines):
     """ Returns a raw list of nodes. Upon completion each node knows who they
         call.
@@ -235,6 +305,7 @@ def build_nodes(lines):
             branch = []
             address = get_function_addr(line)
             node['name'] = get_function_name(line)
+            node['type'] = NodeType.unknown
             node['caller'] = []
 
         elif is_branch(line) and in_progress:
@@ -262,13 +333,67 @@ def set_node_caller(nodes):
                 print("Missing node called by: " + node['name'])
 
 
+def set_node_info(symbols, nodes):
+    
+    for key, symbol in symbols.items():
+        if (symbol['type']) == SymbolType.function:
+            try:
+                nodes[key]['type'] = NodeType.function
+            except KeyError:
+                print("Missing function node: " + symbol['name'])
 
-def build_call_tree(filename):
+
+
+
+def set_dispatch_tables(nodes, lines):
+    """ Upon entry, any free nodes (no caller or branch defined) are potential
+        dispatch tables containing function pointers. Locate dispatch tables
+        and create linkage between function nodes.
+    """
+    for key, node in nodes.items():
+        # Locate free nodes
+        if not node['branch'] and not node['caller']:
+            """ Found a free node.
+                
+                Now locate any references to it,  indicating its a jump table
+            """
+            print(node)
+
+
+def show_node_metrics(nodes):
+    """ Displays node summary information.
+    """
+    leaf_node = 0 # edge of tree
+    free_node = 0 # neither leaf or root, floating
+    root_node = 0 # base of tree
+    unknown_node = 0
+    
+    for key, node in nodes.items():
+        if not node['caller']:
+            root_node += 1
+        
+        if not node['branch']:
+            leaf_node += 1
+        
+        if not node['branch'] and not node['caller']:
+            free_node += 1
+
+        if node['type'] == NodeType.unknown:
+            unknown_node += 1
+
+    print("Root node, total: " + str(root_node) )
+    print("Leaf node, total: " + str(leaf_node) )
+    print("Free node, total: " + str(free_node) )
+    print("Unknown, total: " + str(unknown_node) )
+    print("All nodes, total: " + str(nodes.__len__()) )
+
+
+def build_call_tree(symbols, filename):
     """ Analysis the binary input file and create linkage between symbols (i.e. 
     call tree).
     """
     
-    terminal = subprocess.Popen([cmd, '--disassemble', '--demangle', 
+    terminal = subprocess.Popen([cmd, '--disassemble-all', '--demangle', 
                         #'--start-address=0x08000000', 
                         #'--stop-address=0x08001000',
                         filename ], shell=True, 
@@ -290,37 +415,18 @@ def build_call_tree(filename):
     nodes = {}
     nodes = build_nodes(lines)
     set_node_caller(nodes)
+    set_node_info(symbols, nodes)
+    #set_dispatch_tables(nodes, lines)
 
     #delete_invalid_nodes()
     #insert_vtables()
-    #insert_func_pointers()
     
     #insert_stack_usage()
     
     #build_call_stacks(nodes)
     #visualise_dict()
  
-    leaf_node = 0 # edge of tree
-    free_node = 0 # neither leaf or root, floating
-    root_node = 0 # base of tree
-    
-    for key, node in nodes.items():
-        if not node['caller']:
-            root_node += 1
-            #print("Not called: " + node['name'])
-            #print("Branch: " + str(node['branch']) )
-        
-        if not node['branch']:
-            leaf_node += 1
-        
-        if not node['branch'] and not node['caller']:
-            free_node += 1
-
-    
-    print("Root node, total: " + str(root_node) )
-    print("Leaf node, total: " + str(leaf_node) )
-    print("Free node, total: " + str(free_node) )
-    print("All nodes, total: " + str(nodes.__len__()) )
+    show_node_metrics(nodes)
 
     """ TODO implement long-term solution
     """
@@ -332,8 +438,12 @@ def build_call_tree(filename):
 def main():
     print("Stack Checker")
     filename = validate_input()
+    
+    symbols = {}
+    build_symbol_table(symbols, filename)
+
     #build_symbols(filename)
-    build_call_tree(filename)
+    build_call_tree(symbols, filename)
 
 
 
